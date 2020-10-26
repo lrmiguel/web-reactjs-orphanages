2ffc5b4f8acbb1cf3e10c4fd03ffb6394228d9b0
lrmiguel <leo.rmiguel@gmail.com>
2020-10-26 01:53:50 -0300

    Adding switching pages menu functionality

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,32 +1,54 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import SidebarDashboard from "../components/SidebarDashboard";
 
 import '../styles/pages/dashboard.css';
 import OrphanageCard from "../components/OrphanageCard";
+import MenuContext from "../contexts/MenuContext";
 
 export default function Dashboard() {
+  const { activeMenu } = useContext(MenuContext);
+
   return (
     <div id="page-dashboard">
       <div className="sidebar-container">
         <SidebarDashboard activeScreen='approved-orphanages' />
       </div>
-      <div className="active-screen">
-        <header>
-          <div className="header">
-            <h1>
-              Orfanatos Cadastrados
-            </h1>
-            <p>2 orfanatos</p>
-          </div>
-          <hr />
-          <div className="cards-container">
-            <OrphanageCard />
-            <OrphanageCard />
-            <OrphanageCard />
-          </div>
-        </header>
-      </div>
+      { activeMenu === 'approved-orphanages' ?
+        <div className="active-screen">
+          <header>
+            <div className="header">
+              <h1>
+                Orfanatos Cadastrados
+              </h1>
+              <p>2 orfanatos</p>
+            </div>
+            <hr />
+            <div className="cards-container">
+              <OrphanageCard />
+              <OrphanageCard />
+              <OrphanageCard />
+            </div>
+          </header>
+        </div>
+        :
+        <div className="active-screen">
+          <header>
+            <div className="header">
+              <h1>
+                Cadastros pendentes
+              </h1>
+              <p>2 orfanatos</p>
+            </div>
+            <hr />
+            <div className="cards-container">
+              <OrphanageCard />
+              <OrphanageCard />
+              <OrphanageCard />
+            </div>
+          </header>
+        </div>
+      }
     </div>
   );
 }
